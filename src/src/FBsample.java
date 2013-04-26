a7e4d74ad5225e700069b631daa85f32588304cd
Satoshi Arai <st.arai0227@gmail.com>
2013-04-26 11:48:20 +0900

    Add New java files

---
+++ src/src/FBsample.java
@@ -0,0 +1,41 @@
+package src;
+
+public class FBsample {
+	public static void main(String[] args) {
+		String str = null;
+		String str2 = null;
+
+		int n = 10;
+		n = n * 1;
+
+		if (str instanceof String) {
+			System.out.println("STRING");
+		}
+		switch (n) {
+			case 1 :
+				System.out.println(n);
+			case 2 :
+				System.out.println(n + 1);
+			default :
+				System.out.println(n + 3);
+		}
+
+		String str3 = "Test";
+
+		Boolean bool = true;
+
+		System.out.println(bool);
+
+		int x = 10;
+		if (x <= Integer.MAX_VALUE) {
+			System.out.println("STRINGGG");
+		}
+		boolean ean = true;
+		if (ean = false) {
+			System.out.println("STRING");
+		}
+
+		int b = 100;
+		b = 10;
+	}
+}
