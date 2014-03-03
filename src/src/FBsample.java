ffd516b355d7bbbedc6687ce9e305e44bbbc7008
Satoshi Arai(Lab) <st.arai0227@gmail.com>
2014-03-03 14:28:05 +0900

    Add bug patterns.

--- src/src/FBsample.java
+++ src/src/FBsample.java
@@ -17,16 +17,12 @@ public class FBsample {
 		switch (n) {
 			case 1 :
 				System.out.println(n);
-				break;
 			case 2 :
 				System.out.println(n + 1);
-				break;
 			case 3 :
 				System.out.println(n + 1);
-				break;
 			default :
 				System.out.println(n + 3);
-				break;
 		}
 
 		String str3 = "Test";
